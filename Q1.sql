f7fe43651689939f7c2700924218fb7c479b7645
Stavros Spyrou <63289366+sspyro03@users.noreply.github.com>
2021-11-29 17:54:41 +0200

    Update Q1.sql

--- Q1.sql
+++ Q1.sql
@@ -1,29 +1,35 @@
 CREATE PROCEDURE dbo.Q1
 	@RecordNum		int,
-	@Name			varchar(30),
+	@Name			nvarchar(30),
 	@EntryDate 		date,
-	@username		varchar(30),
-	@password		varchar(30),
-	@FName			varchar(30),
-	@LName			varchar(30),
+	@UserID			int,
+	@username		nvarchar(30),
+	@password		nvarchar(30),
+	@FName			nvarchar(30),
+	@LName			nvarchar(30),
 	@Bdate			date,
 	@Sex			int,
-	@adminUsername 	varchar(30)
+	@adminUsername 	nvarchar(30)
 	
 	AS
 	BEGIN
+
+	DECLARE @adminID AS INT
+		SELECT @adminID = UserID
+						FROM dbo.[p-Users]
+						WHERE Username=@adminUsername
 		SET NOCOUNT ON
 		INSERT INTO dbo.[p-Company](
-			RecordNum,[Name],EntryDate,Created_By
+			RecordNum,[Name],EntryDate,CreatedBy
 		)
 		VALUES(
-			@RecordNum,@Name,@EntryDate,@adminUsername
+			@RecordNum,@Name,@EntryDate,@adminID
 		)
-		INSERT INTO dbo.[p-User](
-			FName,LName ,Bdate, Sex,[Type], Username, Password, CreatedBy, Manages, WorksFor  
+		INSERT INTO dbo.[p-Users](
+			UserID,FName,LName ,Bdate, Sex,[Type], Username, Password, CreatedBy, Manages, WorksFor  
 		)
 		VALUES(
-			@FName,@LName ,@Bdate, @Sex,2, @Username, @Password,@adminUsername, @RecordNum, @RecordNum
+			@UserID,@FName,@LName ,@Bdate, @Sex,2, @Username, @Password, @adminID, @RecordNum, @RecordNum
 		)
 END
 GO
