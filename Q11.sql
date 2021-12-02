92f7eee3bfba9d7caf3a85c3edf3cbf6cb902f98
stytSofo <ssofok02@ucy.ac.cy>
2021-12-02 18:03:39 +0200

    Q11 finished

--- Q11.sql
+++ Q11.sql
@@ -1,4 +1,4 @@
-CREATE PROCEDURE dbo.Q10 @logedInUsername varchar(30),
+CREATE PROCEDURE dbo.Q11 @logedInUsername varchar(30),
 @average int OUTPUT AS BEGIN
 SET
     NOCOUNT ON DECLARE @company int
@@ -10,7 +10,35 @@ SET
             dbo.[p-Users]
         WHERE
             Username = @logedInUsername
-    )
+    ) EXECUTE dbo.Q10 @logedInUsername,
+    @average
+
+SELECT
+    CountQuery.QuestionnaireID
+FROM
+    (
+        SELECT
+            MyQuestionnaires.QuestionnaireID,
+            COUNT(MyQuestionnaires.QuestionnaireID) as QuestionCount
+        FROM
+            (
+                SELECT
+                    dbo.[p-Questionnaire].QuestionnaireID
+                FROM
+                    dbo.[p-Users],
+                    dbo.[p-Questionnaire]
+                WHERE
+                    dbo.[p-Questionnaire].CreatedBy = dbo.[p-Users].UserID
+                    AND dbo.[p-Users].WorksFor = @company
+            ) AS MyQuestionnaires,
+            [p-Includes]
+        WHERE
+            [p-Includes].QuestionnaireID = MyQuestionnaires.QuestionnaireID
+        GROUP BY
+            MyQuestionnaires.QuestionnaireID
+    ) AS CountQuery
+    WHERE CountQuery.QuestionCount > @average
+
 
 
 END RETURN
